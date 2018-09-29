ce9ce01abf1c508cc06263057ba85a94d1e09d73
Luffy <nikhil@134.compilor.com>
2018-09-29 22:57:21 +0530

    Project Finished

---
+++ Coder-SWAG/Models/Products.swift
@@ -0,0 +1,9 @@
+//
+//  Products.swift
+//  Coder-SWAG
+//
+//  Created by Nikhil Muskur on 29/09/18.
+//  Copyright © 2018 Nikhil Muskur. All rights reserved.
+//
+
+import Foundation
---
+++ Coder-SWAG/Views/ProductCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCollectionViewCell.swift
+//  Coder-SWAG
+//
+//  Created by Nikhil Muskur on 29/09/18.
+//  Copyright © 2018 Nikhil Muskur. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
